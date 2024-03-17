3f43a20e9672e3b4aa0e8d14292994cf2864bbc3
Joel Inyang <joelinyang2003@gmail.com>
2024-03-17 19:15:48 +0100

    benmore

--- Projects/settings.py
+++ Projects/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@9%n*-+osiwusvx1#px=f%(dr@1_sp#f1dwya+%x42!)(%gt_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['benmore-project.onrender.com']
 
 
 # Application definition
